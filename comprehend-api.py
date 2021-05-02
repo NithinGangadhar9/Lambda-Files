95691fac659a17fca1088041df2600d9cab43151
NithinGangadhar9 <>
2021-05-01 22:28:47 -0400

    Create comprehend-api.py

---
+++ comprehend-api.py
@@ -0,0 +1,25 @@
+try:
+    import json
+    import boto3
+except Exception as e:
+    print("Some modules are missing {}".format(e))
+
+def lambda_handler(event,context):
+    client_sentiment = boto3.client("comprehend")
+    Text = event.get("Text",None)    
+
+
+
+    response = client_sentiment.detect_sentiment(
+        Text=Text,
+        LanguageCode='en'
+    )
+
+    print (response)
+
+    return {
+        'statusCode' : 200,
+        'body' : json.dumps('Hello from Lambda'),
+        'Response': response
+
+    }
